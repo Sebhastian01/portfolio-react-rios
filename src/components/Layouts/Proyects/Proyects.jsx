e1f3ef473cfc0befe7a08f5258422ad470f455c3
Sebastian Rios Aguirre <sebastian_riosag@soy.sena.edu.co>
2024-10-30 13:53:14 -0500

    fixed button

--- src/components/Layouts/Proyects/Proyects.jsx
+++ src/components/Layouts/Proyects/Proyects.jsx
@@ -18,11 +18,11 @@ export const Proyects = () => {
        <section className="card">
        <div class="card-inner">
         <div className='card-front'>
-        <img  src={pageweb} alt="sebas" />
-        <a href="https://page-of-me.vercel.app/" target="_blank"><button>See more</button></a>
+        <img  src={pageweb} alt="sebas"/><button>Description</button>
         </div>
         <div className='card-back'>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
+          <a href="https://page-of-me.vercel.app/" target="_blank"><button>See more</button></a>
         </div>
         </div>
     </section>
@@ -30,11 +30,11 @@ export const Proyects = () => {
     <section className="card">
     <div class="card-inner">
     <div className='card-front'>
-        <img  src={traveling} alt="sebas" />
-        <a href="https://traveling-amber.vercel.app/" target="_blank"> <button>See more</button></a>
+        <img  src={traveling} alt="sebas" /><button>Description</button>
         </div>
         <div className='card-back'>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
+          <a href="https://traveling-amber.vercel.app/" target="_blank"> <button>See more</button></a>
         </div>
         </div>
     </section>
@@ -42,47 +42,47 @@ export const Proyects = () => {
     <section className="card">
     <div class="card-inner">
     <div className='card-front'>
-        <img  src={atlas} alt="sebas" />
+        <img  src={atlas} alt="sebas" /><button>Description</button>
+        </div>
+        <div className='card-back'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
         <a href="https://atlasproject-nu.vercel.app/" target="_blank"> <button>See more</button></a>
         </div>
-        <div className='card-back'>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
-        </div>
         </div>
     </section> 
 
     <section className="card">
     <div class="card-inner">
     <div className='card-front'>
-        <img  src={finance} alt="sebas" />
+        <img  src={finance} alt="sebas" /><button>Description</button>
+        </div>
+        <div className='card-back'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
         <a href="https://shopping-cart-eta-five-79.vercel.app/" target="_blank"> <button>See more</button></a>
         </div>
-        <div className='card-back'>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
-        </div>
         </div>
     </section> 
 
     <section className="card">
     <div class="card-inner">
     <div className='card-front'>
-        <img  src={store} alt="sebas" />
-        <a href="https://personal-finance-lemon.vercel.app/" target="_blank"> <button>See more</button></a>
+        <img  src={store} alt="sebas" /><button>Description</button>
         </div>
         <div className='card-back'>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
-        </div>
-        </div>
-    </section> 
-
-    <section className="card">
-    <div class="card-inner">
-    <div className='card-front'>
-        <img  src={form} alt="sebas" />
         <a href="https://formulario-js-brown.vercel.app/" target="_blank"> <button>See more</button></a>
         </div>
+        </div>
+    </section> 
+
+    <section className="card">
+    <div class="card-inner">
+    <div className='card-front'>
+        <img  src={form} alt="sebas" /><button>Description</button>
+        </div>
         <div className='card-back'>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae facere quo reprehenderit. Ipsam libero dolorum voluptate totam asperiores magnam cum veniam. Quaerat autem nobis eligendi ipsum id rerum ex dolorum.</p>
+        <a href="https://personal-finance-lemon.vercel.app/" target="_blank"> <button>See more</button></a>
         </div>
         </div>
     </section> 
